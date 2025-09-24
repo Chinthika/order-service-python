8f5f4032f5c106bd429bb0d9c8413a601ad0807f
Chinthika Rathnayake <chinthika.jayani@gmail.com>
2025-09-24 20:56:02 +1000

    Fixed issues with tf

--- Infrastructure/terraform/providers.tf
+++ Infrastructure/terraform/providers.tf
@@ -51,5 +51,10 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.this.token
     config_path            = "~/.kube/config"
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "update-kubeconfig", "--region", var.aws_region, "--name", var.cluster_name]
+      command     = "aws"
+    }
   }
 }
