88b03b307c55ecadb1359bdbae54857f5b4099e8
Chinthika Rathnayake <chinthika.jayani@gmail.com>
2025-09-25 01:05:55 +1000

    Fixed issues

--- Infrastructure/terraform/providers.tf
+++ Infrastructure/terraform/providers.tf
@@ -50,5 +50,6 @@ provider "helm" {
     host                   = data.aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.this.token
+    config_path            = "~/.kube/config"
   }
 }
