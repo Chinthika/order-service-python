3b1a32e556a90ee187e077c862cda3d07d58ebb2
Chinthika Rathnayake <chinthika.jayani@gmail.com>
2025-09-28 09:49:39 +1000

    Fixed tf access issues

--- Infrastructure/terraform/providers.tf
+++ Infrastructure/terraform/providers.tf
@@ -27,6 +27,7 @@ data "aws_eks_cluster_auth" "this" {
 
 provider "helm" {
   kubernetes {
+    load_config_file       = false
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.this.token
