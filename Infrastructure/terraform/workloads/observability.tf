110aab15ec784efebea754a5c755a1ee34b1104e
Chinthika Rathnayake <chinthika.jayani@gmail.com>
2025-09-28 12:20:24 +1000

    Fixed issues with gh workflow

--- Infrastructure/terraform/workloads/observability.tf
+++ Infrastructure/terraform/workloads/observability.tf
@@ -1,10 +1,3 @@
-resource "aws_eks_addon" "metrics_server" {
-  cluster_name = var.cluster_name
-  addon_name   = "metrics-server"
-
-  depends_on = [null_resource.wait_for_cluster]
-}
-
 resource "helm_release" "newrelic" {
   count    = var.deploy_workloads ? 1 : 0
   provider = helm
