c87c83293da6e9fd92a12929b91fd6372f8cbbfc
Chinthika Rathnayake <chinthika.jayani@gmail.com>
2025-09-30 01:51:04 +1000

    Fixed issues

--- Infrastructure/terraform/workloads/observability.tf
+++ Infrastructure/terraform/workloads/observability.tf
@@ -80,12 +80,17 @@ resource "helm_release" "newrelic" {
   }
 
   set {
+    name  = "newrelic-k8s-metrics-adapter.config.externalMetrics.requests_per_pod.removeClusterFilter"
+    value = "true"
+  }
+
+  set {
     name  = "newrelic-k8s-metrics-adapter.config.externalMetrics.service_latency_p95.query"
     value = "FROM Metric SELECT percentile(http.server.duration, 95) WHERE (k8s.serviceName = 'order-service-staging' AND clusterName = 'order-service-shared-eks') SINCE 2 MINUTES AGO"
   }
 
   set {
-    name  = "newrelic-k8s-metrics-adapter.removeClusterFilter"
+    name  = "newrelic-k8s-metrics-adapter.config.externalMetrics.service_latency_p95.removeClusterFilter: true"
     value = "true"
   }
 
