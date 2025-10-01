8f5b5eb5198768204353c1b5d2f4a2c0c8f9021e
Chinthika Rathnayake <chinthika.jayani@gmail.com>
2025-10-01 18:57:28 +1000

    Fixed issues tf

--- Infrastructure/terraform/workloads/backend.tf
+++ Infrastructure/terraform/workloads/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = var.backend_bucket_name
-    key            = var.backend_key_workloads
-    region         = var.backend_region
-    dynamodb_table = var.backend_dynamodb_table
-  }
+  backend "s3" {}
 }
